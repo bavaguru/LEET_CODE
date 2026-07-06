c6d71d5fb2b484a1b231f7fd7907adad5e9e01c8
Bavadharani <bavadharanip2006.ai2023@dce.edu.in>
2026-07-06 15:24:41 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (44.95%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,28 +1,32 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        i = 0
-        n = len(s)
+        s = s.strip()
+
+        if len(s) == 0:
+            return 0
+
         sign = 1
+        i = 0
+
+        if s[0] == "-":
+            sign = -1
+            i = 1
+        elif s[0] == "+":
+            i = 1
+
         num = 0
 
-        while i < n and s[i] == " ":
-            i += 1
-
-        if i < n and (s[i] == "+" or s[i] == "-"):
-            if s[i] == "-":
-                sign = -1
-            i += 1
-
-        while i < n and s[i].isdigit():
-            num = num * 10 + (ord(s[i]) - ord('0'))
-            i += 1
+        for j in range(i, len(s)):
+            if s[j] >= '0' and s[j] <= '9':
+                num = num * 10 + (ord(s[j]) - ord('0'))
+            else:
+                break
 
         num = num * sign
 
-        if num < -2**31:
-            return -2**31
-        if num > 2**31 - 1:
-            return 2**31 - 1
+        if num < -2147483648:
+            return -2147483648
+        if num > 2147483647:
+            return 2147483647
 
         return num
-        
