27d8900b9bff1ee85c35231d9938e35c66e562a6
Bavadharani <bavadharanip2006.ai2023@dce.edu.in>
2026-07-06 14:03:08 +0530

    Time: 282 ms (5.06%) | Memory: 13 MB (46.92%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        cleaned = ""
+        for ch in s:
+            if ch.isalnum():
+                cleaned += ch.lower()
+        return cleaned == cleaned[::-1]
